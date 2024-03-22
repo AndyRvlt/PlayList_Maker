9b00bc39b5d0c7ab5ca845ccc55a23a56017014a
revol <AndyRevolt@yandex.ru>
2024-03-22 23:47:46 +0300

    PlayListMaker/Домашнее задание/Спринт 19/Исправления после первого ревью

--- app/src/main/java/com/example/playlistmaker/search/ui/SearchFragment.kt
+++ app/src/main/java/com/example/playlistmaker/search/ui/SearchFragment.kt
@@ -12,7 +12,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
-import android.widget.*
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
@@ -30,44 +29,13 @@ class SearchFragment : Fragment(), TracksAdapter.TrackListener {
         private const val CLICK_DEBOUNCE_DELAY = 1000L
     }
 
-    private var search: EditText? = null
-    private var clearIcon: ImageView? = null
-    private var searchError: ImageView? = null
-    private var searchPlayList: RecyclerView? = null
-    private var textErrorNothing: TextView? = null
-    private var searchServerError: ImageView? = null
-    private var textServerError: TextView? = null
-    private var update: Button? = null
-    private var storyTrackLiner: LinearLayout? = null
-    private var clearHistorySearch: Button? = null
-    private var historyTrackList: RecyclerView? = null
-    private var progressBar: ProgressBar? = null
-    private var textSearchHistory: TextView? = null
-
     private val searchViewModel by viewModel<SearchViewModel>()
 
     private lateinit var binding: FragmentSearchBinding
 
 
-    private fun initViews() {
-        search = binding.search
-        clearIcon = binding.clearIcon
-        searchPlayList = binding.searchPlayList
-        textErrorNothing = binding.textErrorNothing
-        searchError = binding.searchError
-        searchServerError = binding.searchServerError
-        textServerError = binding.textServerError
-        update = binding.update
-        progressBar = binding.progressBar
-        clearHistorySearch = binding.storyTrack.clearHistorySearch
-        storyTrackLiner = binding.storyTrack.storyTrackLiner
-        historyTrackList = binding.storyTrack.historySearch
-        textSearchHistory = binding.storyTrack.textSearchHistory
-
-    }
-
-    val adapter = TracksAdapter(this) //????
-    val adapterHistory = TracksAdapter(this)//????
+    private val adapter = TracksAdapter(this)
+    private val adapterHistory = TracksAdapter(this)
 
     private var searchText = ""
 
@@ -84,87 +52,91 @@ class SearchFragment : Fragment(), TracksAdapter.TrackListener {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        searchText = savedInstanceState?.getString(SEARCH_TEXT, "") ?: ""
+        searchText = savedInstanceState?.getString(SEARCH_TEXT, "").orEmpty()
 
         searchViewModel.init()
 
-        initViews()
-
-        historyTrackList?.adapter = adapterHistory
-        searchPlayList?.adapter = adapter
-
-
-        clearHistorySearch?.setOnClickListener {
+        binding.storyTrack.clearHistorySearch.setOnClickListener {
             cleanHistory()
         }
 
-        update?.setOnClickListener {
+        binding.storyTrack.historySearch.adapter = adapterHistory
+        binding.searchPlayList.adapter = adapter
+
+        binding.update.setOnClickListener {
             requestTrackList()
         }
 
-        search?.setOnEditorActionListener { _, actionId, _ ->
+        binding.search.setOnEditorActionListener { _, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_DONE) {
                 requestTrackList()
-                searchPlayList?.isVisible = true
-                storyTrackLiner?.isVisible = false
             }
             false
         }
 
-        clearIcon?.setOnClickListener {
+        binding.clearIcon.setOnClickListener {
             searchText = ""
-            search?.setText(searchText)
+            binding.search.setText(searchText)
             val inputMethodManager =
                 requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
-            inputMethodManager?.hideSoftInputFromWindow(clearIcon?.windowToken, 0)
+            inputMethodManager?.hideSoftInputFromWindow(binding.clearIcon.windowToken, 0)
             adapter.updateTracks(emptyList())
+            searchNonWords()
         }
 
         val searchTextWatcher = object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
             }
 
             override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-                clearIcon?.visibility = clearButtonVisibility(s)
-                searchText = search?.text.toString()
+                binding.clearIcon.visibility = clearButtonVisibility(s)
+                searchText = binding.search.text.toString()
                 if (s.isNotEmpty()) {
                     searchDebounce()
+                } else {
+                    searchNonWords()
                 }
             }
 
             override fun afterTextChanged(s: Editable?) {}
         }
-        search?.addTextChangedListener(searchTextWatcher)
+
+        binding.search.addTextChangedListener(searchTextWatcher)
 
         searchViewModel.getInitTracksLiveData().observe(viewLifecycleOwner) {
             adapterHistory.updateTracks(it)
             if (it.isNotEmpty()) {
-                clearHistorySearch?.isVisible = true
-                textSearchHistory?.isVisible = true
+                binding.storyTrack.clearHistorySearch.isVisible = true
+                binding.storyTrack.textSearchHistory.isVisible = true
+                binding.searchError.isVisible = false
+                binding.textErrorNothing.isVisible = false
 
             }
+
         }
 
         searchViewModel.getTracksLiveData()
             .observe(viewLifecycleOwner) { tracks ->
 
-                progressBar?.isVisible = false
+                binding.progressBar.isVisible = false
 
                 adapter.updateTracks(tracks ?: listOf())
                 if (tracks != null) {
                     if (adapter.getTracks().isEmpty()) {
-                        searchPlayList?.isVisible = false
-                        searchError?.isVisible = true
-                        textErrorNothing?.isVisible = true
+                        binding.searchPlayList.isVisible = false
+                        binding.searchError.isVisible = true
+                        binding.textErrorNothing.isVisible = true
 
 
                     } else {
-                        searchPlayList?.isVisible = true
-                        searchError?.isVisible = false
-                        textErrorNothing?.isVisible = false
-                        searchServerError?.isVisible = false
-                        textServerError?.isVisible = false
-                        update?.isVisible = false
+                        binding.searchPlayList.isVisible = true
+                        binding.searchError.isVisible = false
+                        binding.textErrorNothing.isVisible = false
+                        binding.searchServerError.isVisible = false
+                        binding.textServerError.isVisible = false
+                        binding.update.isVisible = false
+                        binding.progressBar.isVisible = false
                     }
                 }
             }
@@ -174,7 +146,7 @@ class SearchFragment : Fragment(), TracksAdapter.TrackListener {
 
             track?.let {
                 adapterHistory.addTrack(track, trackDataHandler)
-                searchViewModel.writeTracks(trackDataHandler) // поменял
+                searchViewModel.writeTracks(trackDataHandler)
             }
         }
     }
@@ -198,6 +170,16 @@ class SearchFragment : Fragment(), TracksAdapter.TrackListener {
         handler.postDelayed(searchRunnable, SEARCH_DEBOUNCE_DELAY)
     }
 
+    private fun searchNonWords() {
+        handler.removeCallbacks(searchRunnable)
+        binding.searchPlayList.isVisible = false
+        binding.storyTrack.historySearch.isVisible = true
+        binding.storyTrack.storyTrackLiner.isVisible = true
+        binding.searchError.isVisible = false
+        binding.textErrorNothing.isVisible = false
+
+    }
+
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putString(SEARCH_TEXT, searchText)
@@ -213,19 +195,21 @@ class SearchFragment : Fragment(), TracksAdapter.TrackListener {
     }
 
     private fun requestTrackList() {
-        progressBar?.isVisible = true
-        searchPlayList?.isVisible = false
-        historyTrackList?.isVisible = false
-        storyTrackLiner?.isVisible = false
+        binding.progressBar.isVisible = true
+        binding.searchPlayList.isVisible = false
+        binding.storyTrack.historySearch.isVisible = false
+        binding.storyTrack.storyTrackLiner.isVisible = false
+        binding.searchError.isVisible = false
+        binding.textErrorNothing.isVisible = false
 
         searchViewModel.getTracks(searchText)
-
     }
+
 
     private fun cleanHistory() {
         searchViewModel.cleanHistory()
         adapterHistory.updateTracks(emptyList())
-        storyTrackLiner?.isVisible = false
+        binding.storyTrack.storyTrackLiner.isVisible = false
     }
 
 
